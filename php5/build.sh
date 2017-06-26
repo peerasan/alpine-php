3c3a904e5f86373a81de142335bcd36c1044471e
peerasan <peerasan.b@dootvmedia.com>
2017-06-26 13:52:47 +0700

    init php 5.6

---
+++ php5/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker stop alpine-php5
+docker rm alpine-php5
+docker rmi alpine-php5
+docker build -t alpine-php5 .
+docker run --name alpine-php5 -d alpine-php5:latest
+docker exec -ti alpine-php5 sh
